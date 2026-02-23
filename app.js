65d0c0ac111a0ea841ecfcea8901007e55cfaeac
yasanjith365 <yasanjith365@gmail.com>
2026-02-23 18:00:38 +0530

    add write to file section

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ fs.readFile("file.txt", "utf8", function (err, data) {
   if (err) throw err;
   console.log(data);
 });
+
+const fs = require("fs");
+
+fs.readFile("file.txt", "utf8", function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
